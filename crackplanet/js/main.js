06d3a1592d24ceb6a6245de865168bc2c42b8240
filzac98 <fiza0001@stud.kea.dk>
2024-05-30 14:09:33 +0200

    Delete crackplanet directory

--- crackplanet/js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const burger = document.getElementById("burger");
-  const navLinks = document.getElementById("navLinks");
-
-  burger.addEventListener("click", () => {
-    burger.classList.toggle("active");
-    navLinks.classList.toggle("active");
-  });
-});
